47e2fe964a1d900de08856d96ad6f8ceeba1b4d1
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-12-10 14:41:19 +0300

    list

--- src/Validator.js
+++ src/Validator.js
@@ -1,6 +1,6 @@
 export default class Validator {
   static validateUserName(userName) {
-    const validSymbolsAndBeforeAfterCheck = /^[^_\-\d][a-zA-Z0-9_\-]+$(?<![_\-\d])/.test(userName);
+    const validSymbolsAndBeforeAfterCheck = /^[^_-\d][a-zA-Z0-9_-]+$(?<![_-\d])/.test(userName);
     const interSymbolsCheck = !/\d{3}/.test(userName);
     return validSymbolsAndBeforeAfterCheck && interSymbolsCheck;
   }
